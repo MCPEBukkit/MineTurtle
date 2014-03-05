135144e611264843e7ae14a4a819bd141170deb4
Andrew Pan <andrew.tianyi.pan@gmail.com>
2014-03-04 21:02:23 -0600

    mmm fresh commits brewing.

--- src/main/java/tk/knownunown/mineturtle/networking/NetworkHandler.java
+++ src/main/java/tk/knownunown/mineturtle/networking/NetworkHandler.java
@@ -4,6 +4,7 @@ import java.net.*;
 import java.io.*;
 import tk.knownunown.mineturtle.Loggy;
 import tk.knownunown.mineturtle.MineTurtle;
+import tk.knownunown.mineturtle.networking.packet.PacketTypes;
 
 /**
  * Created by andrew on 3/1/14.
@@ -32,10 +33,20 @@ public class NetworkHandler extends Thread {
                 Loggy.error("aww, bad luck. IOException?!");
                 e.printStackTrace();
             } finally {
-                String type = Integer.toHexString(0xFF & p.getData()[0]);
+                String hexs = Integer.toHexString(0xFF & p.getData()[0]);
+                int type = (int) Long.parseLong(hexs, 16);
                 Loggy.info("yayz, recieved packet " + type + " with a length of " + p.getLength());
-                //TODO: add a switch statement here to parse packets.
+                //TODO: add a switch statement here to parse packets. (whoops fixed with handlePacket)
+                handlePacket(p, type);
             }
+        }
+    }
+
+    public void handlePacket(DatagramPacket packet, int type){
+        switch(type){ //hah! Tricked you, IntelliJ!
+            case PacketTypes.UNCONNECTED_PING:
+            case PacketTypes.UNCONNECTED_PING_OPEN_CONNECTIONS:
+                break;
         }
     }
 }
---
+++ src/main/java/tk/knownunown/mineturtle/networking/packet/PacketTypes.java
@@ -0,0 +1,12 @@
+package tk.knownunown.mineturtle.networking.packet;
+
+/**
+ * Created by andrew on 3/4/14.
+ */
+public class PacketTypes {
+
+    public static final int UNCONNECTED_PING = 0x01;
+    public static final int UNCONNECTED_PING_OPEN_CONNECTIONS = 0x02;
+    //more later, too lazy now :p
+
+}
